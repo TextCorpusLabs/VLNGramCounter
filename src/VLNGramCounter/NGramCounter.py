b89750ef62760e200c22e9c2de3dd12e01bb34a2
Mark Newman <mark@trinetteandmark.com>
2022-12-28 06:27:23 -0500

    stub in counter

--- src/VLNGramCounter/NGramCounter.py
+++ src/VLNGramCounter/NGramCounter.py
@@ -1,7 +1,22 @@
-
-
-
+from .dtypes import settings
 
 class NGramCounter:
-    def __init__(self, 
+
+    def __init__(self, settings: settings):
+        """
+        Counts large corpuses of ngrams
+
+        Parameters
+        ----------
+        settings : dtypes.settings
+            The settings for the counter
+        """
+        self._settings = settings
+
+    def init(self) -> None:
+        pass
+
+
+    def count(self) -> None:
+        pass
 
